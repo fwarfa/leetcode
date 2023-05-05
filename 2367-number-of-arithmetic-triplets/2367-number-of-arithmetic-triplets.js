80641b07e0969bbc6aa5eb71df0dadfd99decff3
Farah Warfa <fwarfa@gmail.com>
2023-05-05 08:55:23 -0500

    Time: 59 ms (77.69%), Space: 42.2 MB (58.36%) - LeetHub

---
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} diff
+ * @return {number}
+ */
+var arithmeticTriplets = function(nums, diff) {
+        let arithmeticTriplets = 0
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++ ) {
+            if (nums[j] - nums[i] === diff) {
+                for (let k = j + 1; k < nums.length; k++) {
+                    if (nums[k] - nums[j] === diff) {
+                        arithmeticTriplets += 1
+                    }
+                    
+                }
+            }
+
+        }
+    }
+    return arithmeticTriplets
+};
