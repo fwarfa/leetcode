0a7852b0b3d06775526585844819d1c71499b96e
Farah Warfa <fwarfa@gmail.com>
2024-06-20 23:30:05 -0500

    Time: 73 ms (44.36%), Space: 52.9 MB (76.92%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    // if nums.length === 1 || nums.length === 2 return nums
+    
+    // loop through array and make hash map of all numbers and freq in key value
+    let hashMap = {}
+    
+    for (let n of nums) {
+        if (!(n in hashMap)) {
+            hashMap[n] = 1
+        } else {
+            hashMap[n]++
+        }
+    }
+    // sort that by count of most high
+    let sortedEntries = Object.entries(hashMap).sort((a,b) => b[1] - a[1])
+    //  push to new array k times
+    let result = []
+    
+    for (let i = 0; i < k; i++) {
+        result.push(sortedEntries[i][0])
+    }
+    return result
+    
+};
