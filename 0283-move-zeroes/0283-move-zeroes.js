e66be307bc3e8ae127a0fc1f18859d3dc8d17fac
Farah Warfa <fwarfa@gmail.com>
2024-06-24 22:31:18 -0500

    Time: 88 ms (20.54%), Space: 54.8 MB (14.91%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,8 +3,11 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
+    // have 2 pointers
+        // one made loop other outside
+        // both start at 0
+    // if j is a non zero swap with i and increment i
     let i = 0
-    
     for (let j = 0; j < nums.length; j++) {
         if (nums[j] !== 0) {
             [nums[i], nums[j]] = [nums[j], nums[i]]
